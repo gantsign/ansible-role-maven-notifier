3bebdb14620afc5257140814d33e1e274f418b0e
John Freeman <john.freeman.opensource@gmail.com>
2016-09-06 08:16:11 +0100

    Upgraded to Molecule 1.10.3
    Fixed flake8 linter warning.

--- tests/test_role.py
+++ tests/test_role.py
@@ -7,5 +7,6 @@ def test_maven_notifier(Command):
     assert Command("mvn help:help").rc == 0
 
     # Check Maven Notifier installed
-    cmd = Command("find /opt/maven/apache-maven-3.3.9 | grep --color=never 'lib/ext/maven-notifier-'")
+    cmd = Command("find /opt/maven/apache-maven-3.3.9 | grep --color=never %s",
+                  "lib/ext/maven-notifier-")
     assert cmd.rc == 0
