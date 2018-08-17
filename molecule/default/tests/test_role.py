46b9963ad25abf76225c096b69ba8d39fd146f19
John Freeman <john.freeman.opensource@gmail.com>
2018-08-17 18:49:54 +0100

    Changed test to use host fixture (#74)
    Other fixtures are deprecated and will be removed in 2.0 Testinfra release.

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -6,10 +6,9 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_maven_notifier(Command):
-    assert Command("mvn help:help").rc == 0
+def test_maven_notifier(host):
+    assert host.run("mvn help:help").rc == 0
 
     # Check Maven Notifier installed
-    cmd = Command("find /opt/maven/apache-maven-3.3.9 | grep --color=never %s",
-                  "lib/ext/maven-notifier-")
-    assert cmd.rc == 0
+    cmd = "find /opt/maven/apache-maven-3.3.9 | grep --color=never %s"
+    assert host.run(cmd, "lib/ext/maven-notifier-").rc == 0
