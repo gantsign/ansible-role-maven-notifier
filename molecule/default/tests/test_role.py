d36d1d628dee67c616e0af52e9c5ca3e618f063b
John Freeman <john.freeman.opensource@gmail.com>
2019-05-17 18:24:02 +0100

    Updated Maven Notifier to 2.1.0 (#89)
    Keeping up with the latest changes.

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -8,7 +8,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_maven_notifier(host):
     cmd = "mvn '-Djavax.accessibility.assistive_technologies= ' help:help"
-    assert host.run(cmd).rc == 0
+    assert host.run('/bin/bash --login -c %s', cmd).rc == 0
 
     # Check Maven Notifier installed
     cmd = "find /opt/maven/apache-maven-3.3.9 | grep --color=never %s"
