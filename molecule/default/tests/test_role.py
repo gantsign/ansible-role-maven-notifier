1e3a3123fbab60e34ce1e746ed020919780b2e35
John Freeman <john.freeman.opensource@gmail.com>
2020-10-09 17:03:06 +0100

    Updated Molecule to 3.0.8
    Keeping up with the latest changes.

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -1,11 +1,3 @@
-import os
-
-import testinfra.utils.ansible_runner
-
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
-
-
 def test_maven_notifier(host):
     cmd = "mvn '-Djavax.accessibility.assistive_technologies= ' help:help"
     assert host.run('/bin/bash --login -c %s', cmd).rc == 0
